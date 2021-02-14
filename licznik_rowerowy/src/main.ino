e262d5dd0ad96219217196a5b12b0f70a1c65fb9
Michal Czarnecki <mczarnecki11@gmail.com>
2021-02-14 20:11:47 +0100

    Dodano zapis danych GPS do pliku .gpx i rozpoczynanie aktywnosci

--- licznik_rowerowy/src/main.ino
+++ licznik_rowerowy/src/main.ino
@@ -6,6 +6,7 @@
 #include <ILI9341_due.h>
 #include <SystemFont5x7.h>
 #include <SPI.h>
+#include "sd_card.h"
 #include "display.h"
 #include "gps.h"
 
@@ -16,6 +17,9 @@
 int RXPin = A3;
 int TXPin = A2;
 int hall_pin = A1;
+int button_pin = 2;
+int activity_started = 0;
+int button_prev;
 
 int GPSBaud = 9600;
 GPS gps;
@@ -28,12 +32,10 @@ float cadency = 0;
 float avg_cadency = 0;
 int old_state = 0;
 int state = 0;
-bool changed = false;
 float start = 0;
 int count = 0;
 int count_avg = 0;
 float old_rpm = 0;
-int hall_thresh = 10;
 
 int speed_index = 0;
 const int speed_size = 20;
@@ -45,15 +47,15 @@ const int cad_size = 20;
 float cad_array[speed_size];
 bool cad_full = 0;
 
-void displayGpsInfo();
-float countRpm();
-static void smartDelay();
+long int last_gpx = 0;
+
+void createGpx(GPS &gps);
 
 void setup()
 {
   Serial.begin(9600);
   gpsSerial.begin(GPSBaud);
-  
+  pinMode(button_pin, INPUT_PULLUP);
   Wire.begin();
   Compass.SetDeclination(4, 47, 'E');  
   Compass.SetSamplingMode(COMPASS_SINGLE);
@@ -66,42 +68,102 @@ void setup()
 
   display.showGrid();
   display.setFont(SystemFont5x7);
+  button_prev = digitalRead(button_pin);
   display.status(Compass, gps);
   pinMode (hall_pin, INPUT);
+  initializeSdCard();
 }
 
 void loop()
 {
+  gps.getGpsInfo();
+  float speed = gps.getSpd();
   float start_2 = millis();
+  
   while(millis() - start_2 < 500){
     cadency = countRpm();
   }
-  avg_cadency = getAvgCad(cadency);
 
-  for(int i=0;i<cad_size;i++){
-    Serial.print(cad_array[i]);
-    Serial.print(" ");
+  avg_cadency = getAvgCad(cadency);
+  angle = Compass.GetHeadingDegrees();
+  float avg_speed = getAvgSpeed(speed);
+  display.showAvgSpeed(avg_speed);
+  display.status(Compass, gps);
+
+  if(button_prev == HIGH && digitalRead(button_pin) == LOW){
+    startActivity();
   }
-  Serial.println();
+
+  button_prev = digitalRead(button_pin);
+
+  if(activity_started == 1){
+    createGpx(gps);
+  }
 
   display.showAvgCadency(avg_cadency);
   display.showCurCadency(cadency);
-
-  angle = Compass.GetHeadingDegrees();
-
   display.showCompass(angle);
-
-  gps.getGpsInfo();
-
-  float speed = gps.getSpd();
-  float avg_speed = getAvgSpeed(speed);
-  display.showAvgSpeed(avg_speed);
-
   display.showCoords(gps.getLat(), gps.getLon(), gps.getAlt());
   display.showCurSpeed(speed);
-  display.status(Compass, gps);
-
   smartDelay(20);
+}
+
+void stopActivity(){
+  if(activity_started == 1){
+    Serial.println("Activity stopped");
+
+  }
+}
+
+void startActivity(){
+  if(activity_started == 0){
+    Serial.println("Activity started");
+    activity_started = 1;
+  }else{
+    Serial.println("Stop previous activity");
+  }
+}
+
+void createGpx(GPS &gps){
+  char buffer[32];
+  sprintf(
+      buffer,
+      "%02d%02d%02d.gpx",
+      gps.getYear(),
+      gps.getMonth(),
+      gps.getDay());
+
+  myFile = SD.open(buffer, FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if(gps.getLat() != 0){
+    if(millis() - last_gpx >= 8000UL){
+      if (myFile) {
+          Serial.print("Writing to test.txt...");
+          myFile.print("<trkpt lat=\"");
+          myFile.print(gps.getLat(), 10);
+          myFile.print("\" lon=\"");
+          myFile.print(gps.getLon(), 10);
+          myFile.print("\">");
+          myFile.println();
+          myFile.print("<time> ");
+          myFile.print("</time>");
+          myFile.println();
+          myFile.print("<ele> ");
+          myFile.print("</ele>");
+          myFile.println();
+          myFile.print("</trkpt>");
+          myFile.println();
+          last_gpx = millis();
+          Serial.println("done.");
+      } else {
+          // if the file didn't open, print an error:
+          Serial.println("error opening test.txt");
+      }
+    }
+  }
+  myFile.close();
+
 }
 
 float getAvgSpeed(float speed){
@@ -157,14 +219,6 @@ float getAvgCad(float cad){
     return sum/cad_size;
   }
 }
-
-// float countAvgRpm(){
-//   float end_time = millis();
-//   float time_passed = end_time / 1000.0;
-//   float rpm_val = ((count_avg/2) / time_passed) * 60.0;
-
-//   return rpm_val;
-// }
 
 float countRpm(){
   state = digitalRead(hall_pin);
