4ca51fc358e5ba4de3b1e15ade02205d64d61ba4
Michal Czarnecki <mczarnecki11@gmail.com>
2021-02-14 20:10:04 +0100

    Dodano obsluge karty SD

---
+++ licznik_rowerowy/src/sd_card.h
@@ -0,0 +1,38 @@
+// sd_card.h
+#ifndef SD_CARD_H // include guard
+#define SD_CARD_H
+
+#include <SD.h>
+
+File myFile;
+
+void initializeSdCard(){
+    Serial.print("Initializing SD card...");
+    if (!SD.begin(4)) {
+        Serial.println("initialization failed!");
+        while (1);
+    }
+    Serial.println("initialization done.");
+}
+
+void writeToFile(String msg){
+    myFile = SD.open("test.txt", FILE_WRITE);
+
+    // if the file opened okay, write to it:
+    if (myFile) {
+        Serial.print("Writing to test.txt...");
+        myFile.println(msg);
+        // close the file:
+        myFile.close();
+        Serial.println("done.");
+    } else {
+        // if the file didn't open, print an error:
+        Serial.println("error opening test.txt");
+    }
+}
+
+
+
+
+
+#endif /* SD_CARD_H */
